23745d2f42e73d5a4d2b53d88cfb209978633cc2
appacmp <appacmp@appacmps-MacBook-Pro.local>
2015-07-19 14:05:15 +0200

    Initial Commit

---
+++ ToDoList/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  ToDoList
+//
+//  Created by appacmp on 19/07/15.
+//  Copyright (c) 2015 appcamp. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
